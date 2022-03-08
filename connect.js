67cadf9cf9bc649cdee06e2fd4db125a6d382fff
Emeric <emeric.l@apps-panel.com>
2022-03-08 12:01:31 +0100

    heroku fix

--- connect.js
+++ connect.js
@@ -3,6 +3,9 @@ require("dotenv/config")
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: process.env.DB_DIALECT,
+    dialectOptions : {
+        ssl: true,
+    },
     logging: false
 })
 
